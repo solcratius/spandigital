0382691dfa2f6f649979c3cc2b6de1a1d3df78fa
System Administrator <root@Jamess-MacBook-Pro.local>
2017-02-28 19:58:26 -0500

    config updated

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'span_digital');
+define('DB_NAME', 'spandigital');
 
 /** MySQL database username */
 define('DB_USER', 'root');
